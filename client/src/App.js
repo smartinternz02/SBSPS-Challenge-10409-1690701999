9b7acd4c53faa5168b9d4d37dbaf6ecb0e3ef0dc
amandhariwal2626 <a.dhariwal12345@gmail.com>
2023-07-31 20:07:39 +0530

    modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/App.js 	modified:   client/src/index.js 	new file:   client/src/pages/SignIn.js 	new file:   client/src/pages/SignUp.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import SignIn from './pages/SignIn.js'
+import SignUp from './pages/SignUp.js';
+import {Routes,BrowserRouter,Route} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' exact element={<SignIn/>} />
+    <Route path='/register' exact element={<SignUp/>} />
+   </Routes>
+   </BrowserRouter>
   );
 }
 
