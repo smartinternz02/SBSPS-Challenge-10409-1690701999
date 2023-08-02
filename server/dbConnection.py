6f645ea1458bcc2292bb7fd9ac9bb53900c89e05
amandhariwal2626 <64463821+amandhariwal2626@users.noreply.github.com>
2023-08-02 16:36:43 +0530

    Update dbConnection.py
    revoked accidently provided ibm db2 credential

--- server/dbConnection.py
+++ server/dbConnection.py
@@ -3,7 +3,7 @@ import ibm_db
 app = Flask(__name__)
 
 # SECURITY=SSL;SSLCertificate=DigiCertGlobalRootCA.crt;
-conn = ibm_db.connect('DATABASE=bludb; HOSTNAME=fbd88901-ebdb-4a4f-a32e-9822b9fb237b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud; PORT=32731; UID=mkz61721; PWD=4foCRQCQKktx2Gy7','','')
+conn = ibm_db.connect('DATABASE=DB_NAME; HOSTNAME=HOST_NAME; PORT=PORT_NO; UID=USER_ID; PWD=PASSWORD','','')
 connState = ibm_db.active(conn)
 print (connState)
 
