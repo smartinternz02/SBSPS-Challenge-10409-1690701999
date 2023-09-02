4cbb4c821cde2088eea881b4bbdb0578643f4104
amandhariwal2626 <a.dhariwal12345@gmail.com>
2023-09-02 16:20:50 +0530

    Minor changes

--- server/app.py
+++ server/app.py
@@ -4,20 +4,22 @@ import requests
 import os 
 import ibm_db
 import bcrypt
-# from flask_bcrypt import Bcrypt
 from dotenv import load_dotenv
 from flask_cors import CORS
+import json
 
 app=Flask(__name__)
 CORS(app)
 api = Api(app)
-# bcrypt2 = Bcrypt(app)
+
 
 # Establish a connection to the DB2 database
 conn = ibm_db.connect('DATABASE=bludb; HOSTNAME=fbd88901-ebdb-4a4f-a32e-9822b9fb237b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud; PORT=32731; UID=mkz61721; PWD=4foCRQCQKktx2Gy7; Security=SSL; SSLCertificate=DigiCertGlobalRootCA.crt','','')
 connState = ibm_db.active(conn)
-print (connState)
 
+
+OPENCHARGEMAP_API_KEY = os.getenv("OPENCHARGEMAP_API_KEY")
+OPENCHARGEMAP_API_BASE_URL = 'https://api.openchargemap.io/v3/poi/'
 
 # Helper function to check if a user exists in the database
 def user_exist(username):
@@ -51,6 +53,7 @@ def registration():
 
 
 
+
 # Resource for login authentication and retrieving data from db using SQL query
 @app.route('/login', methods=['POST'])
 def login():
@@ -74,8 +77,119 @@ def login():
         return jsonify({"message": "Invalid Credentials"}), 401
 
 
-OPENCHARGEMAP_API_KEY = os.getenv("OPENCHARGEMAP_API_KEY")
-OPENCHARGEMAP_API_BASE_URL = 'https://api.openchargemap.io/v3/poi/'
+
+
+
+# Function to fetch and update data from the API
+def fetch_and_update_data():
+    # Fetch data from the OpenChargeMap referencedata endpoint
+    referencedata_url = f'https://api.openchargemap.io/v3/referencedata?key={OPENCHARGEMAP_API_KEY}'
+    referencedata_response = requests.get(referencedata_url)
+    referencedata = referencedata_response.json()
+    # print(referencedata)
+
+    # Check if ChargerTypes table exists, create if not
+    charger_types_table_check_sql = "SELECT COUNT(*) FROM sysibm.systables WHERE name = 'CHARGERTYPES'"
+    stmt = ibm_db.exec_immediate(conn, charger_types_table_check_sql)
+    result = ibm_db.fetch_assoc(stmt)
+    charger_types_table_exists = result['1'] > 0
+    # print(charger_types_table_exists)
+
+    if not charger_types_table_exists:
+        # Create ChargerTypes table if it doesn't exist
+        charger_types_table_create_sql = """
+        CREATE TABLE ChargerTypes (
+            ID INT PRIMARY KEY NOT NULL,
+            Comments VARCHAR(255),
+            IsFastChargeCapable BOOLEAN,
+            Title VARCHAR(255)
+        )
+        """
+        ibm_db.exec_immediate(conn, charger_types_table_create_sql)
+
+    # Check if ConnectionTypes table exists, create if not
+    connection_types_table_check_sql = "SELECT COUNT(*) FROM sysibm.systables WHERE name = 'CONNECTIONTYPES'"
+    stmt = ibm_db.exec_immediate(conn, connection_types_table_check_sql)
+    result = ibm_db.fetch_assoc(stmt)
+    connection_types_table_exists = result['1'] > 0
+
+    if not connection_types_table_exists:
+        # Create ConnectionTypes table if it doesn't exist
+        connection_types_table_create_sql = """
+        CREATE TABLE ConnectionTypes (
+            ID INT PRIMARY KEY NOT NULL,
+            FormalName VARCHAR(255),
+            IsDiscontinued BOOLEAN,
+            IsObsolete BOOLEAN,
+            Title VARCHAR(255)
+        )
+        """
+        ibm_db.exec_immediate(conn, connection_types_table_create_sql)
+
+    # Insert or update data in ChargerTypes table
+    for charger_type in referencedata['ChargerTypes']:
+        charger_type_id = charger_type['ID']
+        charger_type_insert_update_sql = f"""
+        MERGE INTO ChargerTypes AS target
+        USING (SELECT {charger_type_id} AS ID, '{charger_type["Comments"]}' AS Comments, {charger_type["IsFastChargeCapable"]} AS IsFastChargeCapable, '{charger_type["Title"]}' AS Title FROM sysibm.sysdummy1) AS source
+        ON target.ID = source.ID
+        WHEN MATCHED THEN
+            UPDATE SET Comments = source.Comments,
+                       IsFastChargeCapable = source.IsFastChargeCapable,
+                       Title = source.Title
+        WHEN NOT MATCHED THEN
+            INSERT (ID, Comments, IsFastChargeCapable, Title)
+            VALUES (source.ID, source.Comments, source.IsFastChargeCapable, source.Title)
+        """
+        ibm_db.exec_immediate(conn, charger_type_insert_update_sql)
+
+# Insert or update data in ConnectionTypes table
+    for connection_type in referencedata['ConnectionTypes']:
+        connection_type_id = connection_type['ID']
+        
+        # Handle potential None values for other fields
+        formal_name = connection_type.get("FormalName", "")  # Provide an empty string as a default value
+        is_discontinued = connection_type.get("IsDiscontinued", False)  # Provide False as a default value
+        is_obsolete = connection_type.get("IsObsolete", False)  # Provide False as a default value
+        title = connection_type.get("Title", "")  # Provide an empty string as a default value
+        
+        # Convert boolean values to integers (0 or 1) to match DB2 BOOLEAN data type
+        is_discontinued = int(is_discontinued) if is_discontinued is not None else 0
+        is_obsolete = int(is_obsolete) if is_obsolete is not None else 0
+        
+        connection_type_insert_update_sql = f"""
+        MERGE INTO ConnectionTypes AS target
+        USING (
+            SELECT
+                {connection_type_id} AS ID,
+                '{formal_name}' AS FormalName,
+                {is_discontinued} AS IsDiscontinued,
+                {is_obsolete} AS IsObsolete,
+                '{title}' AS Title
+            FROM sysibm.sysdummy1
+        ) AS source
+        ON target.ID = source.ID
+        WHEN MATCHED THEN
+            UPDATE SET
+                FormalName = source.FormalName,
+                IsDiscontinued = source.IsDiscontinued,
+                IsObsolete = source.IsObsolete,
+                Title = source.Title
+        WHEN NOT MATCHED THEN
+            INSERT (ID, FormalName, IsDiscontinued, IsObsolete, Title)
+            VALUES (source.ID, source.FormalName, source.IsDiscontinued, source.IsObsolete, source.Title)
+        """
+        ibm_db.exec_immediate(conn, connection_type_insert_update_sql)
+
+
+
+
+# Add a route to trigger data fetching and updating
+@app.route('/fetch-and-update-data', methods=['GET'])
+def fetch_and_update_data_route():
+    fetch_and_update_data()
+    return jsonify({"message": "Data fetched and updated successfully"}), 200
+
 
 
 def fetch_charging_stations(latitude, longitude, max_results=100):
@@ -85,10 +199,14 @@ def fetch_charging_stations(latitude, longitude, max_results=100):
 
 @app.route('/charging-stations')
 def charging_stations():
-    latitude = 12.971599 #40.7128
-    longitude = 77.594566 #-74.0060
+    latitude = 12.9141 #40.7128
+    longitude = 74.8560 #-74.0060
     charging_stations_data = fetch_charging_stations(latitude,longitude)
     return jsonify(charging_stations_data)
 
 if __name__=="__main__":
+    # Check if the database connection is active
+    if connState:
+        # Call fetch_and_update_data only when the database connection is established
+        fetch_and_update_data()
     app.run(debug=True)
